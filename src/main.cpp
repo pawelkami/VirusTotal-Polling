3cbed9546c9f664fcb29e0a4049850751894f6d9
Carlos Zaldivar Batista <carlos.zaldivar.batista@gmail.com>
2016-05-22 14:49:51 +0200

    Dodano wczytywanie argumentów z terminala.

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <boost/program_options.hpp>
+#include <vector>
 #include "Configuration.h"
 #include "Logger.h"
 #include "HttpClient.h"
@@ -6,6 +8,40 @@ using namespace std;
 
 int main(int argc, char** argv)
 {
+    namespace po = boost::program_options;
+    po::options_description description("Options");
+    description.add_options()
+            ("help,h", "Print this message")
+            ("cycles,c", "Number of rescans")
+            ("time,t", "Time between rescans")
+            ("file", po::value<string>(), "File to scan");
+
+    po::positional_options_description positionalDescription;
+    positionalDescription.add("file", 1);
+
+
+    po::variables_map vm;
+    try
+    {
+        po::store(po::command_line_parser(argc, argv).options(description).positional(positionalDescription).run(),
+                  vm);
+        po::notify(vm);
+
+        if (vm.count("help"))
+        {
+            std::cout << description << std::endl;
+            return 0;
+        }
+
+    }
+    catch(po::error& e)
+    {
+        std::cerr << "ERROR: " << e.what() << std::endl << std::endl;
+        std::cerr << description << std::endl;
+        return 0;
+    }
+
+
     HttpClient http;
 
     HttpRequest request;
